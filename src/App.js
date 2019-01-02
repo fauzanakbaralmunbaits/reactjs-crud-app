ec62946159cbca80dfbddc16bf270c42bfff46e0
Fauzan Akbar Almunbaits <fauzanakbaralmunbaits@gmail.com>
2019-01-02 14:21:34 +0700

    add, edit, delete fix

--- src/App.js
+++ src/App.js
@@ -18,37 +18,79 @@ class App extends Component {
   }
 
   fSubmit = (e) =>{
-    e.prefentDefault();
+    e.preventDefault();
     console.log('try');
 
     let datas = this.state.datas;
-    let name = this.refs.name.values;
-    let address = this.refs.address.values;
+    let name = this.refs.name.value;
+    let address = this.refs.address.value;
 
-    let data = {
-      name, address
+    if(this.state.act === 0){ //new data
+
+      let data = {
+        name, address
+      }
+      datas.push(data);
+
+    }else{                    //update data
+
+      let index = this.state.index;
+      datas[index].name = name;
+      datas[index].address = address;
+
     }
-    datas.push(data);
 
     this.setState({
-      datas:datas
+      datas:datas,
+      act:0
     });
 
     this.refs.myForm.reset();
     this.refs.name.focus();
   }
 
+  fRemove = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fEdit = (i) =>{
+    let data = this.state.datas[i];
+    this.refs.name.value = data.name;
+    this.refs.address.value = data.address;
+
+    this.setState({
+      act:1,
+      index:i
+    });
+
+    this.refs.name.focus();
+  }
+
   render() {
+    let datas = this.state.datas;
     return (
       <div className="App">
         <h2>{this.state.title}</h2>
         <form ref="myForm" className="myForm">
           <input type="text" ref="name" placeholder="Your Name" className="formField" />
           <input type="text" ref="address" placeholder="Your Address" className="formField" />
-          <button onClick={this.fSubmit} className="myButton">Submit</button>
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton">Submit</button>
         </form>
         <pre>
-          
+          {datas.map((data, i)=>
+              <li key={i} className="myList">
+              {i+1}. {data.name}, {data.address}
+              <button onClick={()=>this.fRemove(i)} className="myButton">Remove</button>
+              <button onClick={()=>this.fEdit(i)} className="myButton">Edit</button>
+              </li>
+            )}
         </pre>
       </div>
     );
